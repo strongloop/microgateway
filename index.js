cc88a98bc75205a3cb2465491f0be1d46d375378
librah <librah@tw.ibm.com>
2015-12-11 09:34:09 +0800

    Adds context, preflow, and assembly middleware.

---
+++ index.js
@@ -0,0 +1,37 @@
+/*
+ * Populate APIm context and determine the API swagger based
+ * on the clientID, http method, and URI
+ */
+'use strict';
+var debug = require('debug')('strong-gateway:preflow');
+
+module.exports = function createPreflowMiddleware(options) {
+  debug('configuration', options);
+  
+  // TODO - assembly is currently hardcoded. 
+  // To be replaced with file or Jon's config-mgmt model
+
+  return function preflow(req, res, next) {
+    // if the URL doesn't being with /apim, then skip the preflow
+    // the reason is not to break existing StrongGateway's test cases
+    if (req.originalUrl.search(/^apim\//) == -1) {
+      next();
+    }
+
+    let ctx = req.ctx;
+
+    let assembly = 
+        'assembly:\n' +
+        '  execute:\n' +
+        '    - invoke-api:\n' +
+        '        target-url: "http://$(target-host)/$(request.path)"\n'+
+        '        verb: $(request.verb)\n';
+
+    ctx.set('flowAssembly', require('yamljs').parse(assembly));
+    ctx.set('target-host', 'localhost:8889');
+    ctx.set('request.path', req.originalUrl);
+    ctx.set('request.verb', req.method);
+    next();
+  };
+};
+
