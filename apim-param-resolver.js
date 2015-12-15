3da4c28ec344c7516de7f3e7b4c496f116c64937
librah <librah@tw.ibm.com>
2015-12-15 14:51:15 +0800

    Code change per Raymond's review comments
    - update flow-engine's URL in package.json
    - add comment to apim-param-resolver.js to describe 
      its function
    - move some one-time codes out of the middleware 
      function so it only gets executed once
    - replace 'let' with 'var' declaration

--- apim-param-resolver.js
+++ apim-param-resolver.js
@@ -1,5 +1,19 @@
 /*
- *  APIm assembly YAML placeholder replacement
+ *  This module provides function to replace APIm context 
+ *  variables (syntax: $(varname)) configured on a policy
+ *  with the actual context variable value.
+ *
+ *  For example, 
+ *  - invoke-api:
+ *      url: "https://$(target-host)/services/climbing/$(reqest.path)
+ *
+ *  The url after replaced will be:
+ *      url: "https://somehost/sevices/climbing/apim/stockQuote
+ *
+ *  if APIm context has the following two variables:
+ *   - target-host: somehost
+ *   - request.path: apim/stockQuote
+ *
  */
 'use strict';
 var debug = require('debug')('strong-gateway:apim-param-resolver');
@@ -7,7 +21,7 @@ var util = require('util');
 
 module.exports = function(context, name, value) {
   if (util.isString(value)) {
-    let newValue = value.replace(/\$\(([^)]+)\)/gm, function(m, g1) {
+    var newValue = value.replace(/\$\(([^)]+)\)/gm, function(m, g1) {
         return context.get(g1);
     }); 
     debug('replace parameter "'+name+'": "'+value+'" with "'+ newValue + '"');
--- index.js
+++ index.js
@@ -5,12 +5,13 @@
 var debug = require('debug')('strong-gateway:assembly');
 
 var paramResolver = require('./apim-param-resolver');
+var FlowCtor = require('flow-engine').Flow;
 
 module.exports = function createAssemblyMiddleware(options) {
   debug('configuration', options);
 
   return function assembly(req, res, next) {
-    let ctx = req.ctx;
+    var ctx = req.ctx;
     // if there is no assembly defined, skip the flow-engine
     if (ctx === undefined || ctx.get('flowAssembly') === undefined) {
       debug('Skip ' + req.originalUrl + ' non-apim traffics');
@@ -21,12 +22,11 @@ module.exports = function createAssemblyMiddleware(options) {
     debug('ctx.flowAssembly: ' + 
             JSON.stringify(ctx.get('flowAssembly'), undefined, 2));
 
-    const FlowCtor = require('flow-engine').Flow;
-    let options = {};
+    var options = {};
     options.paramResolver = paramResolver;
     options.context = ctx;
 
-    let flow = new FlowCtor(ctx.get('flowAssembly'), options);
+    var flow = new FlowCtor(ctx.get('flowAssembly'), options);
     flow.prepare(ctx, next);
     flow.run();
   };
