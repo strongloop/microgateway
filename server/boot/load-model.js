1cfb6beea31cdd9d79c9543291dda33204ecbbab
Jon Palgon <palgon@us.ibm.com>
2016-01-12 15:01:34 -0500

    Minor improvements to loading models
    1. Remove unnecessary use of async module
    2. Make population of future models more programmatic instead of ‘copy
    & paste’ (only one place needs updating when creating a new model)

--- server/boot/load-model.js
+++ server/boot/load-model.js
@@ -1,115 +1,77 @@
 var fs = require('fs');
 var path = require('path');
-var async = require('async');
 var debug = require('debug')('strong-gateway:data-store');
+
+/**
+ * Creates a model type 
+ * @class
+ * @param {string} name - name of the model
+ * @param {string} prefix - file name prefix associated with the model
+ */ 
+function modelType(name, prefix) {
+    this.name = name;
+    this.prefix = prefix;
+    this.files = [];
+}
 
 module.exports = function(app) {
 
     var files = fs.readdirSync(__dirname);
-
     debug('files: ', files);
-    var apifiles = [];
-    var productfiles = [];
-    var subscriptionfiles = [];
-    var catalogfiles = [];
-    async.each(files, function(file, done) {
-        if (file.indexOf('apis-') > -1)
-                {
-                debug('apis file: ', file);
-                apifiles.push(file);
+
+    // Associate models with file names containing data that should be used
+    // to populate the relevant model(s)
+    // This section would need to be updated whenever new models are added
+    // to the data-store
+    var models = [];
+    models[models.length] = new modelType('catalog',
+                                       'catalogs-');
+    models[models.length] = new modelType('product', 
+                                       'products-');
+    models[models.length] = new modelType('api',
+                                       'apis-');
+    models[models.length] = new modelType('subscription',
+                                       'subs-');
+
+    // read the content of the files into memory
+    files.forEach(
+        function(file) {
+            for(var i = 0; i < models.length; i++) {
+                if(file.indexOf(models[i].prefix) > -1) {
+                    debug('%s file: %s', models[i].name, file);
+                    models[i].files.push(file);
+                    break;
                 }
-        if (file.indexOf('products-') > -1)
-                {
-                debug('products file: ', file);
-                productfiles.push(file);
+            }
+        }
+    );
+
+    // populate data-store models with the file contents
+    models.forEach(
+        function(model) {
+            model.files.forEach(
+                function(typefile) {
+                    var file = path.join(__dirname, typefile);
+                    debug('Loading data from %s', file);
+                    var readfile = JSON.parse(fs.readFileSync(file));
+                    debug('filecontents: ', readfile);
+                    app.dataSources.db.automigrate(
+                        model.name,
+                        function(err) {
+                            if (err) throw err;
+                            app.models[model.name].create(
+                                readfile,
+                                function(err, mymodel) {
+                                    if (err) throw err;
+                                    debug('%s created: %j',
+                                          model.name,
+                                          mymodel);
+                                }
+                            );
+                        }
+                    );
                 }
-        if (file.indexOf('subs-') > -1)
-                {
-                debug('subscriptions file: ', file);
-                subscriptionfiles.push(file);
-                }
-        if (file.indexOf('catalogs-') > -1)
-                {
-                debug('catalogs file: ',  file);
-                catalogfiles.push(file);
-                }
-        });
-
-    // Populate catalogs
-    async.each(catalogfiles, function(catalogfile, done) {
-        var file = path.join(__dirname, catalogfile);
-        debug('Loading data from %s', file);
-        var readcatalog = JSON.parse(fs.readFileSync(file));
-        debug('filecontents: ', readcatalog);
-        app.dataSources.db.automigrate('catalog', function(err) {
-                if (err) throw err;
-
-                app.models.catalog.create(readcatalog, function(err, catalogs) {
-                        if (err) throw err;
-
-                        debug('catalogs created: %j', catalogs);
-
-                        });
-
-                });
-        });
-
-    // Populate products
-    async.each(productfiles, function(productfile, done) {
-        var file = path.join(__dirname, productfile);
-        debug('Loading data from %s', file);
-        var readproduct = JSON.parse(fs.readFileSync(file));
-        debug('filecontents: ', readproduct);
-        app.dataSources.db.automigrate('product', function(err) {
-                if (err) throw err;
-
-                app.models.product.create(readproduct, function(err, products) {
-                        if (err) throw err;
-
-                        debug('products created: %j', products);
-
-                        });
-
-                });
-        });
-
-    // Populate apis
-    async.each(apifiles, function(apifile, done) {
-        var file = path.join(__dirname, apifile);
-        debug('Loading data from %s', file);
-        var readapi = JSON.parse(fs.readFileSync(file));
-        debug('filecontents: ', readapi);
-	app.dataSources.db.automigrate('api', function(err) {
-    		if (err) throw err;
-
-    		app.models.api.create(readapi, function(err, apis) {
-      			if (err) throw err;
-
-      			debug('apis created: %j', apis);
-
-    			});
-
-        	});
-	});
-
-    // Populate subscriptions
-    async.each(subscriptionfiles, function(subscriptionfile, done) {
-        var file = path.join(__dirname, subscriptionfile);
-        debug('Loading data from %s', file);
-        var readsubscription = JSON.parse(fs.readFileSync(file));
-        debug('filecontents: ' + readsubscription);
-        app.dataSources.db.automigrate('subscription', function(err) {
-                if (err) throw err;
-
-                app.models.subscription.create(readsubscription, 
-			function(err, subscriptions) {
-                        if (err) throw err;
-
-                        debug('subscriptions created: %j', subscriptions);
-
-                        });
-
-                });
-        });
-
+            );
+        }
+    );
 };
