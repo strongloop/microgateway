c83788527551b2a11c2d204fe7ee5a543f1a6c01
librah <librah@tw.ibm.com>
2016-01-18 19:11:59 +0800

    Enable context middleware to run with different options
    Allow user to customize context middleware behavior via
    define options in `server/middleware.json` file.
    User will be able to override:
     - request.content-type normalization
     - message.body parsing
     - date / time format
    
    Also let message.body data type determined by
    the normalized request.content-type value.

--- index.js
+++ index.js
@@ -13,15 +13,19 @@ var debug = require('debug')('strong-gateway:context');
 //
 // internal module dependencies
 //
-var populateMessageVariables = require('./lib/populate-message-variables.js')();
-var populateRequestVariables = require('./lib/populate-request-variables.js')();
-var populateSystemVariables  = require('./lib/populate-system-variables.js')();
 
 
 module.exports = function createContextMiddleware(options) {
   debug('configuration', options);
 
   options = options || {};
+
+  var populateMessageVariables =
+          require('./lib/populate-message-variables.js')(options.message);
+  var populateRequestVariables =
+          require('./lib/populate-request-variables.js')(options.request);
+  var populateSystemVariables  =
+          require('./lib/populate-system-variables.js')(options.system);
 
   return function(req, res, next) {
     // create the APIm context used for the following middlewares
