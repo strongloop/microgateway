7d0c9f2d3bb516173118bba0a5ddea263dfa0a02
John C. Bellessa <jcbelles@us.ibm.com>
2016-02-09 11:07:50 -0500

    Added simple test cases for basic auth

--- test/basic.policy.test.js
+++ test/basic.policy.test.js
@@ -11,9 +11,16 @@ describe('basic auth policy', function() {
 
   let request;
   before((done) => {
+    console.log('Starting micro-gateway');
     mg.start(3000)
-      .then(() => ldap.start(1389))
-      .then(() => echo.start(8889))
+      .then(() => {
+        console.log('Starting LDAP server');
+        return ldap.start(1389);
+      })
+      .then(() => {
+        console.log('Starting Echo server');
+        return echo.start(8889);
+      })
       .then(() => {
         request = supertest('http://localhost:3000');
         console.log ('setup test1');
@@ -32,13 +39,21 @@ describe('basic auth policy', function() {
       .catch(done);
   });
 
-  var clientId1 = 'fb82cb59-ba95-4c34-8612-e63697d7b845';
-  it('test basic auth',
-     function(done) {
-       console.log ('send request');
-       request
-         .post('/apim/sb/v1/ascents?client_id=' +  clientId1)
-         .send({date: 'today', route: '66'})
-         .expect(200, '{"date":"today","route":"66"}', done);
-     });
+  let clientId1 = 'fb82cb59-ba95-4c34-8612-e63697d7b845';
+  it(`client_id=${clientId1} should pass with "root"/"Hunter2"`, function(done) {
+    console.log ('send request');
+    request
+      .post('/apim/sb/v1/ascents?client_id=' +  clientId1)
+      .auth('root', 'Hunter2')
+      .send({date: 'today', route: '66'})
+      .expect(200, '{"date":"today","route":"66"}', done);
+  });
+
+  let clientId2 = '612caa59-9649-491f-99b7-d9a941c4bd2e';
+  it(`client_id=${clientId2} should fail`, function(done) {
+    request
+      .get('/apim/sb/v1/forecasts?client_id=' + clientId2)
+      .auth('root', 'wrongpassword')
+      .expect(401, '', done);
+    });
 });
