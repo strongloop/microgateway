ce98017918734b17cea713742fc75e650780eba0
Jon Palgon <palgon@us.ibm.com>
2016-02-09 11:12:35 -0500

    Allow for specification of APIm port
    APIMANAGER_PORT environment variable or config file entry

--- datastore/server/boot/load-model.js
+++ datastore/server/boot/load-model.js
@@ -6,8 +6,9 @@ var debug = require('debug')('strong-gateway:data-store');
 var sgwapimpull = require('../../apim-pull');
 var apimpull = sgwapimpull.pull;
 var environment = require('../../../utils/environment');
-var APIMANAGER = require('../../../utils/environment').APIMANAGER;
-var CONFIGDIR = require('../../../utils/environment').CONFIGDIR;
+var APIMANAGER = environment.APIMANAGER;
+var APIMANAGER_PORT = environment.APIMANAGER_PORT;
+var CONFIGDIR = environment.CONFIGDIR;
 
 var rootConfigPath = '/../../../config/';
 var defaultDefinitionsDir = __dirname + rootConfigPath + 'default';
@@ -56,7 +57,7 @@ module.exports = function(app) {
   models.push(new ModelType('optimizedData', 'dummy'));
   models.push(new ModelType('snapshot', 'dummy')); // hack, removed later
 
-  var apimanager;
+  var apimanager = {};
 
   async.series(
     [
@@ -84,10 +85,20 @@ module.exports = function(app) {
         environment.getVariable(
           APIMANAGER,
           function(value) {
-            apimanager = value;
-            if (apimanager)
+            apimanager.ip = value;
+            if (apimanager.ip)
               laptopexperience=false;
-            },
+          },
+          callback
+        );
+      },
+      function(callback) {
+        // get apimanager port
+        environment.getVariable(
+          APIMANAGER_PORT,
+          function(value) {
+            apimanager.port = value;
+          },
           callback
         );
       },
@@ -105,10 +116,10 @@ module.exports = function(app) {
     function(err, results) {
       if (!err) {
         loadData(app,
-                    apimanager,
-                    models,
-                    definitionsDir,
-                    true); // first call to loadData()
+                 apimanager,
+                 models,
+                 definitionsDir,
+                 true); // first call to loadData()
       }
     }
   );
@@ -220,7 +231,8 @@ function pullFromAPIm(apimanager, uid, cb) {
         }
 
         var options = {};
-        options['host'] = apimanager;
+        options['host'] = apimanager.ip;
+        options['port'] = apimanager.port;
         options['outdir'] = snapdir;
         debug('apimpull start');
         apimpull(options,function(err, response) {
