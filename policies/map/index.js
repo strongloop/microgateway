1519ad1ff8db04d2651da70915417d543d8fdaca
Daniel Badt <badt@us.ibm.com>
2016-02-14 15:19:03 -0500

    Revert "Initial (empty shell) map policy implementation"
    This reverts commit d36d24f82f4432b2f795bd45f20bc9da65276010.

--- policies/map/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var debug = require('debug')('policy:map');
-module.exports = function(config) {
-  return function(props, context, next) {
-    debug('ENTER map');
-    console.log('do some map stuff');
-    next();
-  };
-};
