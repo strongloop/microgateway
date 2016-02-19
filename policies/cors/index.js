3dffef5d4c791d4071652cbb9222fd59c8e48839
Anthony Ffrench <tffrench@us.ibm.com>
2016-02-19 17:40:49 -0500

    explicit CORS policy support

--- policies/cors/index.js
+++ policies/cors/index.js
@@ -1,9 +1,34 @@
 'use strict';
 var debug = require('debug')('policy:cors');
 module.exports = function(config) {
-  return function(props, context, next) {
-    debug('ENTER cors');
-    console.log('do some cors stuff');
+  return function(props, ctx, next) {
+    ctx._flow.subscribe('FINISH', (event, next) => {
+      let allowCreds = props['allow-credentials'];
+      if (allowCreds === true) {
+        ctx.message.headers['Access-Control-Allow-Credentials'] = 'true';
+      }
+      let allowHeaders = props['allow-headers'];
+      if (allowHeaders) {
+        ctx.message.headers['Access-Control-Allow-Headers'] = allowHeaders;
+      }
+      let allowMethods = props['allow-methods'];
+      if (allowMethods) {
+        ctx.message.headers['Access-Control-Allow-Methods'] = allowMethods;
+      }
+      let allowOrigin = props['allow-origin'];
+      if (allowOrigin) {
+        ctx.message.headers['Access-Control-Allow-Origin'] = allowOrigin;
+      }
+      let exposeHeaders = props['expose-headers'];
+      if (exposeHeaders) {
+        ctx.message.headers['Access-Control-Expose-Headers'] = exposeHeaders;
+      }
+      let maxAge = props['max-age'];
+      if (maxAge) {
+        ctx.message.headers['Access-Control-Max-Age'] = maxAge.toString();
+      }
+      next();
+    });
     next();
   };
 };
