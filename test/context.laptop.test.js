53ee0696d403cf17afed80f831cf30602359941b
librah <librah@tw.ibm.com>
2016-02-22 15:30:10 +0800

    Fix context UT test cases

--- test/context.laptop.test.js
+++ test/context.laptop.test.js
@@ -1,6 +1,9 @@
 'use strict';
 
+let _ = require('lodash');
+let assert = require('assert');
 let supertest = require('supertest');
+
 let mg = require('../lib/microgw');
 
 describe('Context variables', function() {
@@ -31,7 +34,80 @@ describe('Context variables', function() {
   it('should produce all $(api) context variables', function(done) {
     request
       .get('/context/api')
-      .expect(200, {}, done); // TODO: fix this, there should be body returned
+      .expect(200)
+      .end(function(err, res) {
+        var result = res.body;
+        if (_.isString(result)) {
+          result = JSON.parse(result);
+        }
+
+        result.endpoint.address = '*';
+
+        assert.deepStrictEqual(result, {
+          basepath: '/',
+          endpoint: {
+            address: '*',
+            hostname: 'localhost'
+          },
+          id: 'context:1.0.0',
+          method: 'GET',
+          name: 'context',
+          operationId: 'getAPI',
+          path: '/context/api',
+          properties: {
+            foo: 'default_foo'
+          },
+          type: 'REST',
+          version: '1.0.0'
+        });
+        done();
+      });
+  });
+
+  it('should produce all $(_.api) context variables', function(done) {
+    request
+      .get('/context/internal')
+      .expect(200)
+      .end(function(err, res) {
+        var result = res.body;
+        if (_.isString(result)) {
+          console.log('parsing JSON');
+          result = JSON.parse(result);
+        }
+
+        assert.deepStrictEqual(result, {
+          consumes: [
+            'application/xml',
+            'application/json'
+          ],
+          operation: 'GET',
+          parameters: [
+            {
+                description: 'parameter 1',
+                in: 'query',
+                name: 'param1',
+                type: 'string'
+            },
+            {
+                description: 'parameter 2',
+                in: 'header',
+                name: 'param2',
+                type: 'integer'
+            }
+          ],
+          path: '/context/internal',
+          produces: [
+            'application/xml',
+            'application/json'
+          ],
+          responses: {
+            '200': {
+              description: "200 OK"
+            }
+          }
+        });
+        done();
+      });
   });
 
 });
