ff411b3466444848bcf733b0a96f5d74d3345fe5
John C. Bellessa <jcbelles@us.ibm.com>
2016-02-25 18:50:04 -0500

    Updated ldap-server to only bring up TLS server when given a TLS socket

--- test/support/ldap-server/index.js
+++ test/support/ldap-server/index.js
@@ -24,20 +24,25 @@ exports.start = function (port, tlsport) {
       server.listen(port, resolve);
     }))
     .then(() => new Promise(resolve => {
-      tlsserver.listen(tlsport || port+1, resolve);
-      resolve();
+      if (tlsport)
+        tlsserver.listen(tlsport, resolve);
+      else {
+        tlsserver = null;
+        resolve();
+      }
     }));
 };
 
 exports.stop = function() {
   return Promise.resolve().then(() => {
     server.close();
-    tlsserver.close();
+    if (!!tlsserver)
+      tlsserver.close();
   });
 };
 
 if (require.main === module) {
-  exports.start(1389).
+  exports.start(1389, 1636).
     then(() => {
       console.log('ldap-server started on port 1389');
     });
