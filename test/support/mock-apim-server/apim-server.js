95cbc0ed45c73e4eb0fc36bf0c66a6cf39702a91
librah <librah@tw.ibm.com>
2016-03-01 11:23:48 +0800

    Make mock-apim-server support custom data folder and promize

--- test/support/mock-apim-server/apim-server.js
+++ test/support/mock-apim-server/apim-server.js
@@ -5,34 +5,60 @@ var  fs = require('fs')
   , host = '127.0.0.1';
 
 
-var options = {
-  key: fs.readFileSync(__dirname + '/key.pem'),
-  cert: fs.readFileSync(__dirname + '/cert.pem')
+var server;
+
+/**
+ * Start a mock apim server. Two ways to start it:
+ *  - start(host, port, path, cb)
+ *  - start(host, port, cb)
+ *
+ * @param host {string} hostname
+ * @param port {integer} port number
+ * @param path {string} optional, specifying the path to load apim config files
+ */
+exports.start = function (host, port, path) {
+  return new Promise((resolve, reject) => {
+    path = path || __dirname;
+
+    var options = {
+      key: fs.readFileSync(__dirname + '/key.pem'),
+      cert: fs.readFileSync(__dirname + '/cert.pem')
+    };
+
+    var files = new static.Server(path);
+
+    function serveFiles(request, response) {
+      files.serve(request, response, function (err, res) {
+          if (err) {
+            console.error('> Error serving ' + request.url + ' - ' + err.message);
+            response.writeHead(err.status, err.headers);
+            response.end();
+          } else {
+            console.log('> ' + request.url + ' - ' + res.message);
+          }
+        }
+      );
+    }
+
+    server = https.createServer(options, serveFiles);
+    server.listen(port, host, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log('mock-apim-server listening on port ' + port);
+        resolve();
+      }
+    });
+
+  });
 };
 
-var files = new static.Server(__dirname);
-
-function serveFiles(request, response) {
-  files.serve(request, response, function (err, res) {
-      if (err) {
-        console.error('> Error serving ' + request.url + ' - ' + err.message);
-        response.writeHead(err.status, err.headers);
-        response.end();
-      } else {
-        console.log('> ' + request.url + ' - ' + res.message);
-      }
-    }
-  );
-}
-
-exports.start = function (host, port, cb) {
-  var server = https.createServer(options, serveFiles);
-  server.on('listening', function(err) {
-      if (cb) cb(err);
-    }
-  );
-  server.listen(port,host);
-  return server;
+exports.stop = function() {
+  return new Promise((resolve, reject) => {
+    server.close(() => {
+      resolve(0);
+    })
+  });
 };
 
 //exports.start(host,port);
