aad95e5ccd1a3adf35f7a38170eb07503e66bb20
Jon Palgon <palgon@us.ibm.com>
2016-03-01 17:40:11 -0500

    Add datastore tests for automatic refresh

--- datastore.test.js
+++ datastore.test.js
@@ -11,7 +11,7 @@ let apimServer = require('./support/mock-apim-server/apim-server');
 
 describe('data-store', function() {
   let request;
-  let snapshotID, oldSnapshotID;
+  let snapshotID;
   before((done) => {
     process.env.DATASTORE_PORT = 5000;
     process.env.APIMANAGER_PORT = 8890;
@@ -37,39 +37,22 @@ describe('data-store', function() {
     delete process.env.NODE_ENV;
     microgw.stop()
       .then(() => echo.stop())
-      .then(() => apimServer.stop())
       .then(done, done)
       .catch(done);
   });
 
   function verifyResponseArray(res, expected) {
     assert.strictEqual(res.length, expected.length);
-    var current = -1;
-    var usedvalues = Array(res.length).fill(false);
 
     for(var i = 0; i < expected.length; i++) {
       var expect = expected[i];
-      for (var j = 0; j < res.length; j++) {
-        if (usedvalues[j] === true)
-          continue;
-        if(_.isMatch(res[j], expect)) {
-          var actual = res[j];
-          usedvalues[j] = true;
-          for (var prop in expect) {
-            if (expect.hasOwnProperty(prop)) {
-              assert.strictEqual(actual[prop], expect[prop]);
-            }
-          }
-          if(current === -1 && actual.current === true) {
-            current = j;
-          }
+      var actual = res[i];
+      for (var prop in expect) {
+        if (expect.hasOwnProperty(prop)) {
+          assert.strictEqual(actual[prop], expect[prop]);
         }
       }
     }
-    for(var k = 0; k < usedvalues.length; k++) {
-      assert(usedvalues[k] === true);
-    }
-    return current;
   }
 
   function verifyResponseSingle(res, expected) {
@@ -120,7 +103,7 @@ describe('data-store', function() {
             if (err) return done(err);
             setTimeout(
               done,
-              20000 // 15 seconds to ensure second snapshot begins
+              15000 // 15 seconds to ensure second snapshot begins
             );
           }
         );
@@ -134,56 +117,28 @@ describe('data-store', function() {
       request
         .get('/api/snapshots')
         .expect(function(res) {
-            var curr = verifyResponseArray(res.body, expect);
-            var old = curr > 0 ? 0 : 1;
-            oldSnapshotID = snapshotID;
-            snapshotID = res.body[curr].id;
-            assert(res.body[old].id === oldSnapshotID);
-            assert(oldSnapshotID !== snapshotID);
-            assert(snapshotID.length === 5); // ID's are strings of 5 characters
-            assert(parseInt(snapshotID) >= 0); // ID's are >= 0
-            assert(parseInt(snapshotID) < 65536); // ID's are < 65536
+            console.log(res.body);
+            verifyResponseArray(res.body, expect);
+            snapshotID = res.body[0].id;
+            assert(res.body[0].id === snapshotID);
+            assert(res.body[1].id !== snaphostID);
+            assert(res.body[1].id.length === 5); // ID's are strings of 5 characters
+            assert(parseInt(res.body[1].id) >= 0); // ID's are >= 0
+            assert(parseInt(res.body[1].id) < 65536); // ID's are < 65536
           }
         ).end(done);
     }
   );
-  it('release should return old snapshot and decrement ref count',
+  it('release should return current snapshot and decrement ref count',
     function(done) {
       var expect = {refcount : '1', current: false};
       request
-        .get('/api/snapshots/release?id=' + oldSnapshotID)
+        .get('/api/snapshots/release?id=' + snapshotID)
         .expect(function(res) {
             verifyResponseSingle(res.body.snapshot, expect);
-            assert(res.body.snapshot.id === oldSnapshotID); // ID should be same as previous
+            assert(res.body.snapshot.id === snapshotID); // ID should be same as previous
           }
         ).end(done);
-    }
-  );
-  it('release should remove old snapshot and decrement ref count and cleanup dir',
-    function(done) {
-      var expect = {snapshot : {}};
-      request
-        .get('/api/snapshots/release?id=' + oldSnapshotID)
-        .expect(function(res) {
-            assert(_.isEqual(expect, res.body));
-            
-          }
-        ).end(function (err, res) {
-            if (err) return done(err);
-            setTimeout(
-              function () {
-                // check for non existence of directory
-                try {
-                  var stats = fs.statSync(process.env['ROOTCONFIGDIR'] + oldSnapshotID);
-                } catch (e) {
-                  if(e.code === 'ENOENT') return done(); // expected
-                }
-                done(new Error('Snapshot directory still exists'));
-              },
-              1500 // 1.5 seconds to cleanup
-            );
-          }
-        );
     }
   );
   it('release should remove current snapshot and decrement ref count and cleanup dir',
