e5b34d4b5e7edd3c0c621cad9ff85ac6d9f26817
juangmj <juangmj@tw.ibm.com>
2016-03-07 19:37:27 +0800

    update the files under test/ for downgrade to node 0.10.x

--- test/score.test.js
+++ test/score.test.js
@@ -10,27 +10,27 @@ var fs = require('fs');
 describe('matching score test', function() {
 
   var request;
-  before((done) => {
+  before(function(done) {
     process.env.APIMANAGER = '127.0.0.1';
     process.env.NODE_ENV = 'production';
     mg.start(3000)
-      .then(() => echo.start(8889))
-      .then(() => {
+      .then(function() { return echo.start(8889); })
+      .then(function() {
         request = supertest('http://localhost:3000');
         console.log ('setup test1');
       })
       .then(done)
-      .catch((err) => {
+      .catch(function(err) {
         console.error(err);
         done(err);
       });
   });
 
-  after((done) => {
+  after(function(done) {
     delete process.env.APIMANAGER;
     delete process.env.NODE_ENV;
     echo.stop()
-      .then(() => mg.stop())
+      .then(function() { return mg.stop(); })
       .then(done, done);
   });
 
