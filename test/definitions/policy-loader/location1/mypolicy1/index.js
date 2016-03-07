b1be5b98de5890d7756596088883c2dfbaf81cd8
Raymond Feng <rfeng@us.ibm.com>
2016-03-07 09:56:35 -0800

    Fix tests with ES5.x

--- test/definitions/policy-loader/location1/mypolicy1/index.js
+++ test/definitions/policy-loader/location1/mypolicy1/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const assert = require('assert');
+var assert = require('assert');
 
 module.exports = function(config) {
     return function(props, context, next) {
--- test/urlrewrite.test.js
+++ test/urlrewrite.test.js
@@ -1,37 +1,37 @@
 'use strict';
 
-let express = require('express');
-let supertest = require('supertest');
-let echo = require('./support/echo-server');
-let mg = require('../lib/microgw');
+var express = require('express');
+var supertest = require('supertest');
+var echo = require('./support/echo-server');
+var mg = require('../lib/microgw');
 var path = require('path');
 var fs = require('fs');
 
 describe('urlrewrite', function() {
 
-  let request;
-  before((done) => {
+  var request;
+  before(function(done) {
     process.env.APIMANAGER = '127.0.0.1';
     process.env.NODE_ENV = 'production';
     process.env.WLPN_APP_ROUTE = 'http:///apim/sb'
     mg.start(3000)
-      .then(() => echo.start(8889))
-      .then(() => {
+      .then(function() {echo.start(8889);})
+      .then(function() {
         request = supertest('http://localhost:3000');
         console.log ('setup test1');
       })
       .then(done)
-      .catch((err) => {
+      .catch(function(err) {
         console.error(err);
         done(err);
       });
   });
 
-  after((done) => {
+  after(function(done) {
     delete process.env.APIMANAGER;
     delete process.env.NODE_ENV;
     echo.stop()
-      .then(() => mg.stop())
+      .then(function() {mg.stop();})
       .then(done, done);
   });
 
