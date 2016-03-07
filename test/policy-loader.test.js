449b1c31765e11bc4dd83e2069e2ff01c6343e8b
YI-HONG WANG <yhwang@tw.ibm.com>
2016-03-07 16:04:55 +0800

    back to ecam5

--- test/policy-loader.test.js
+++ test/policy-loader.test.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const fs        = require('fs');
-const express   = require('express');
-const supertest = require('supertest');
-const echo      = require('./support/echo-server');
-const policyLoader  = require('../lib/policy-loader');
-const should        = require('should');
-const path          = require('path');
+var fs        = require('fs');
+var express   = require('express');
+var supertest = require('supertest');
+var echo      = require('./support/echo-server');
+var policyLoader  = require('../lib/policy-loader');
+var should        = require('should');
+var path          = require('path');
 
 describe('policy-loader', function() {
     describe('load default policy', function() {
         it('should contains cors, invoke, redaction set-variable', function() {
-            let pl = policyLoader.create(path.resolve(__dirname, '..', 'policies'));
+            var pl = policyLoader.create(path.resolve(__dirname, '..', 'policies'));
             pl.should.be.a.Object();
-            let policies = pl.getPolicies();
+            var policies = pl.getPolicies();
             policies.should.have.property('cors');
             policies.should.have.property('invoke');
             policies.should.have.property('set-variable');
@@ -24,15 +24,15 @@ describe('policy-loader', function() {
     });
     describe('multiple locations', function() {
         it('should load policies in location1 and location2', function() {
-            let paths = [
+            var paths = [
                 path.resolve(__dirname, 'definitions',
                         'policy-loader', 'location1'),
                 path.resolve(__dirname, 'definitions',
                         'policy-loader', 'location2')
             ];
-            let pl = policyLoader.create(paths);
+            var pl = policyLoader.create(paths);
             pl.should.be.a.Object();
-            let policies = pl.getPolicies();
+            var policies = pl.getPolicies();
             policies.should.have.property('mypolicy1');
             policies.should.have.property('mypolicy2');
             policies.should.have.property('mypolicy3');
@@ -41,7 +41,7 @@ describe('policy-loader', function() {
             policies['mypolicy2'].should.be.a.Function();
             policies['mypolicy3'].should.be.a.Function();
             policies['mypolicy4'].should.be.a.Function();
-            let context = {};
+            var context = {};
             function next() {};
             policies.mypolicy1({}, context, next);
             //the second mypolicy1 override the first one
@@ -68,19 +68,19 @@ describe('policy-loader', function() {
     });
 
     describe('use APIC_CONFIG_PATH to load policies', function() {
-        before((done) => {
+        before(function(done) {
           process.env.APIC_CONFIG_PATH = __dirname + '/definitions/policy-loader';
           done();
         });
 
-        after((done) => {
+        after(function(done) {
           delete process.env.APIC_CONFIG_PATH;
           done();
         });
 
         it('should load user policies in config correctly', function(done) {
             var loader = policyLoader.createMGLoader();
-            let policies = loader.getPolicies();
+            var policies = loader.getPolicies();
             policies.should.have.property('cors');
             policies.should.have.property('invoke');
             policies.should.have.property('set-variable');
@@ -96,7 +96,7 @@ describe('policy-loader', function() {
             policies['mypolicy3'].should.be.a.Function();
             policies['mypolicy4'].should.be.a.Function();
 
-            let context = {};
+            var context = {};
             function next() {};
             policies.mypolicy1({}, context, next);
             //the second mypolicy1 override the first one
@@ -106,7 +106,7 @@ describe('policy-loader', function() {
 
         it('should load user policies in config and disable override', function(done) {
             var loader = policyLoader.createMGLoader({'override':false});
-            let policies = loader.getPolicies();
+            var policies = loader.getPolicies();
             policies.should.have.property('cors');
             policies.should.have.property('invoke');
             policies.should.have.property('set-variable');
@@ -122,7 +122,7 @@ describe('policy-loader', function() {
             policies['mypolicy3'].should.be.a.Function();
             policies['mypolicy4'].should.be.a.Function();
 
-            let context = {};
+            var context = {};
             function next() {};
             policies.mypolicy1({}, context, next);
             //the second mypolicy1 can't override the first one
@@ -135,19 +135,19 @@ describe('policy-loader', function() {
 
         var cwd = process.cwd();
         var testdir = path.resolve(__dirname, 'definitions', 'policy-loader');
-        before((done) => {
+        before(function(done) {
             process.chdir(testdir);
             done();
         });
 
-        after((done) => {
+        after(function(done) {
           process.chdir(cwd);
             done();
         });
 
         it('should load user policies in config correctly', function(done) {
             var loader = policyLoader.createMGLoader();
-            let policies = loader.getPolicies();
+            var policies = loader.getPolicies();
             policies.should.have.property('cors');
             policies.should.have.property('invoke');
             policies.should.have.property('set-variable');
@@ -162,7 +162,7 @@ describe('policy-loader', function() {
             policies['mypolicy2'].should.be.a.Function();
             policies['mypolicy3'].should.be.a.Function();
 
-            let context = {};
+            var context = {};
             function next() {};
             policies.mypolicy1({}, context, next);
             //the second mypolicy1 override the first one
