064a6866f6a867bd3fc10762a15c72691700e3ef
Ryan Graham <rmg@ca.ibm.com>
2016-03-19 12:16:38 -0700

    insert copyright headers

--- datastore/server/boot/root.js
+++ datastore/server/boot/root.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: apiconnect-microgateway
+// US Government Users Restricted Rights - Use, duplication or disclosure
+// restricted by GSA ADP Schedule Contract with IBM Corp.
+
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
