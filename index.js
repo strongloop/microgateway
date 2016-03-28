9f8d94c58989c0cb98a49c3e665fbe76cf20322f
Jeremy Geddes <jgeddes@us.ibm.com>
2016-03-28 08:43:01 -0700

    add default onprem prof json.. fix typo.. make production default

--- index.js
+++ index.js
@@ -3,4 +3,7 @@
 // US Government Users Restricted Rights - Use, duplication or disclosure
 // restricted by GSA ADP Schedule Contract with IBM Corp.
 
+process.env.NODE_ENV = 'production';
 require('./lib/microgw.js').start(process.env.PORT || 5000);
+
+console.log('process.env.NODE_ENV: ' + process.env.NODE_ENV);
