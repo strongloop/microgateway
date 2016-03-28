e7adc6c3d888be01174928b9503d47c3ed91713e
Jeremy Geddes <jgeddes@us.ibm.com>
2016-03-28 09:12:58 -0700

    only set if undefined

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 // Node module: apiconnect-microgateway
 // US Government Users Restricted Rights - Use, duplication or disclosure
 // restricted by GSA ADP Schedule Contract with IBM Corp.
-
-process.env.NODE_ENV = 'production';
+if (process.env.NODE_ENV === undefined)
+  process.env.NODE_ENV = 'production';
 require('./lib/microgw.js').start(process.env.PORT || 5000);
-
