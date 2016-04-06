bcc7798457c5b3ae7970cb2cfdc660a838d0ed99
Jon Palgon <palgon@us.ibm.com>
2016-04-06 14:57:02 -0400

    Initial UT

---
+++ test/https.test.js
@@ -0,0 +1,351 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: apiconnect-microgateway
+// US Government Users Restricted Rights - Use, duplication or disclosure
+// restricted by GSA ADP Schedule Contract with IBM Corp.
+
+'use strict';
+
+var _ = require('lodash');
+var assert = require('assert');
+var supertest = require('supertest');
+var echo = require('./support/echo-server');
+var yaml = require('yamljs');
+var apimServer = require('./support/mock-apim-server/apim-server');
+
+var mg = require('../lib/microgw');
+
+describe('HTTP and HTTPS in separate files', function() {
+
+  var request, httprequest;
+  before(function(done) {
+    process.env.CONFIG_DIR = __dirname + '/definitions/https/combined1';
+    process.env.NODE_ENV = 'production';
+    process.env.APIMANAGER = '127.0.0.1';
+    process.env.APIMANAGER_PORT = 8081;
+    apimServer.start(
+            process.env.APIMANAGER,
+            process.env.APIMANAGER_PORT,
+            process.env.CONFIG_DIR)
+        .then(done)
+        .catch(function(err) {
+            console.error(err);
+            done(err);
+            });
+  });
+
+  after(function(done) {
+    delete process.env.CONFIG_DIR;
+    delete process.env.NODE_ENV;
+    delete process.env.APIMANAGER;
+    delete process.env.APIMAMANGER_PORT;
+    apimServer.stop()
+      .then(done, done)
+      .catch(done);
+  });
+
+  it('should expect failure to load', function(done) {
+    mg.start(3000)
+      .catch(function(err) {
+        assert(true);
+        done();
+      });
+  });
+
+});
+/*
+describe('HTTP and HTTPS in same file', function() {
+
+  var request, httprequest;
+  before(function(done) {
+    process.env.CONFIG_DIR = __dirname + '/definitions/https/combined2';
+    process.env.NODE_ENV = 'production';
+    done();
+  });
+
+  after(function(done) {
+    delete process.env.CONFIG_DIR;
+    delete process.env.NODE_ENV;
+    done();
+  });
+
+  it('should expect failure to load', function(done) {
+    mg.start(3000)
+      .catch(function(err) {
+        assert(true);
+        done();
+      });
+  });
+
+});
+
+describe('HTTPS in laptop experience w/ env var', function() {
+
+  var request, httprequest;
+  before(function(done) {
+    process.env.CONFIG_DIR = __dirname + '/definitions/https/httpsexplicit';
+    process.env.NODE_ENV = 'production';
+    process.env.TLS_SERVER_CONFIG = __dirname + '/support/https/tlsconfig.json'
+    mg.start(3000)
+      .then(function() {
+        return echo.start(8889);
+      })
+      .then(function() {
+        request = supertest(mg.app);
+        httprequest = supertest('http://localhost:3000');
+      })
+      .then(done)
+      .catch(function(err) {
+        console.error(err);
+        done(err);
+      });
+  });
+
+  after(function(done) {
+    mg.stop()
+      .then(function() { return echo.stop(); })
+      .then(done, done)
+      .catch(done);
+    delete process.env.CONFIG_DIR;
+    delete process.env.NODE_ENV;
+    delete process.env.TLS_SERVER_CONFIG;
+  });
+
+  it('should expect success', function(done) {
+    request
+      .get('/https/https')
+      .expect(200, done);
+  });
+  it('should expect failure', function(done) {
+    httprequest
+      .get('/http/http')
+      .end(function(err, res) {
+        if (err) return done(); // expect error
+        done(new Error('expect error'));
+      });
+  });
+
+});
+
+describe('HTTPS in laptop experience w/ pfx', function() {
+
+  var request, httprequest;
+  before(function(done) {
+    process.env.CONFIG_DIR = __dirname + '/definitions/https/httpsexplicit';
+    process.env.NODE_ENV = 'production';
+    process.env.TLS_SERVER_CONFIG = __dirname + '/support/https/tlsconfig-pfx.json'
+    mg.start(3000)
+      .then(function() {
+        return echo.start(8889);
+      })
+      .then(function() {
+        request = supertest(mg.app);
+        httprequest = supertest('http://localhost:3000');
+      })
+      .then(done)
+      .catch(function(err) {
+        console.error(err);
+        done(err);
+      });
+  });
+
+  after(function(done) {
+    mg.stop()
+      .then(function() { return echo.stop(); })
+      .then(done, done)
+      .catch(done);
+    delete process.env.CONFIG_DIR;
+    delete process.env.NODE_ENV;
+    delete process.env.TLS_SERVER_CONFIG;
+  });
+
+  it('should expect success', function(done) {
+    request
+      .get('/https/https')
+      .expect(200, done);
+  });
+  it('should expect failure', function(done) {
+    httprequest
+      .get('/http/http')
+      .end(function(err, res) {
+        if (err) return done(); // expect error
+        done(new Error('expect error'));
+      });
+  });
+
+});
+
+describe('HTTPS in laptop experience w/ default TLS', function() {
+
+  var request, httprequest;
+  before(function(done) {
+    process.env.CONFIG_DIR = __dirname + '/definitions/https/httpsexplicit';
+    process.env.NODE_ENV = 'production';
+    mg.start(3000)
+      .then(function() {
+        return echo.start(8889);
+      })
+      .then(function() {
+        request = supertest(mg.app);
+        httprequest = supertest('http://localhost:3000');
+      })
+      .then(done)
+      .catch(function(err) {
+        console.error(err);
+        done(err);
+      });
+  });
+
+  after(function(done) {
+    mg.stop()
+      .then(function() { return echo.stop(); })
+      .then(done, done)
+      .catch(done);
+    delete process.env.CONFIG_DIR;
+    delete process.env.NODE_ENV;
+  });
+
+  it('should expect success', function(done) {
+    request
+      .get('/https/https')
+      .expect(200, done);
+  });
+  it('should expect failure', function(done) {
+    httprequest
+      .get('/http/http')
+      .end(function(err, res) {
+        if (err) return done(); // expect error
+        done(new Error('expect error'));
+      });
+  });
+
+});
+
+describe('HTTP in laptop experience when HTTPS not specified', function() {
+
+  var request, httprequest;
+  before(function(done) {
+    process.env.CONFIG_DIR = __dirname + '/definitions/https/http';
+    process.env.NODE_ENV = 'production';
+    mg.start(3000)
+      .then(function() {
+        return echo.start(8889);
+      })
+      .then(function() {
+        httprequest = supertest('http://localhost:3000');
+      })
+      .then(done)
+      .catch(function(err) {
+        console.error(err);
+        done(err);
+      });
+  });
+
+  after(function(done) {
+    mg.stop()
+      .then(function() { return echo.stop(); })
+      .then(done, done)
+      .catch(done);
+    delete process.env.CONFIG_DIR;
+    delete process.env.NODE_ENV;
+  });
+
+  it('should expect success', function(done) {
+    httprequest
+      .get('/http/http')
+      .expect(200, done);
+  });
+
+});
+
+describe('HTTPS in laptop experience when HTTPS explicitly specified', function() {
+
+  var request, httprequest;
+  before(function(done) {
+    process.env.CONFIG_DIR = __dirname + '/definitions/https/httpsexplicit';
+    process.env.NODE_ENV = 'production';
+    mg.start(3000)
+      .then(function() {
+        return echo.start(8889);
+      })
+      .then(function() {
+        request = supertest(mg.app);
+        httprequest = supertest('http://localhost:3000');
+      })
+      .then(done)
+      .catch(function(err) {
+        console.error(err);
+        done(err);
+      });
+  });
+
+  after(function(done) {
+    mg.stop()
+      .then(function() { return echo.stop(); })
+      .then(done, done)
+      .catch(done);
+    delete process.env.CONFIG_DIR;
+    delete process.env.NODE_ENV;
+  });
+
+  it('should expect success', function(done) {
+    request
+      .get('/https/https')
+      .expect(200, done);
+  });
+  it('should expect failure', function(done) {
+    httprequest
+      .get('/http/http')
+      .end(function(err, res) {
+        if (err) return done(); // expect error
+        done(new Error('expect error'));
+      });
+  });
+
+});
+
+describe('HTTPS in laptop experience when schemes not specified', function() {
+
+  var request, httprequest;
+  before(function(done) {
+    process.env.CONFIG_DIR = __dirname + '/definitions/https/httpsexplicit';
+    process.env.NODE_ENV = 'production';
+    mg.start(3000)
+      .then(function() {
+        return echo.start(8889);
+      })
+      .then(function() {
+        request = supertest(mg.app);
+        httprequest = supertest('http://localhost:3000');
+      })
+      .then(done)
+      .catch(function(err) {
+        console.error(err);
+        done(err);
+      });
+  });
+
+  after(function(done) {
+    mg.stop()
+      .then(function() { return echo.stop(); })
+      .then(done, done)
+      .catch(done);
+    delete process.env.CONFIG_DIR;
+    delete process.env.NODE_ENV;
+  });
+
+  it('should expect success', function(done) {
+    request
+      .get('/https/https')
+      .expect(200, done);
+  });
+  it('should expect failure', function(done) {
+    httprequest
+      .get('/http/http')
+      .end(function(err, res) {
+        if (err) return done(); // expect error
+        done(new Error('expect error'));
+      });
+  });
+
+});
+*/
