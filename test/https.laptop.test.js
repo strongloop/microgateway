c105ac772aea57dd11078fec1eac8f9c8fcadcc4
Behnam Hajian <behnam@ca.ibm.com>
2016-07-13 16:01:43 -0400

    microgateway: support password obfuscation
    Since WAS requires liberty members to obfuscate keystore
    password when storing it on the disk, and microgateway is
    using that passphrase, microgateway needs to de-obfuscate
    the password.

--- test/https.laptop.test.js
+++ test/https.laptop.test.js
@@ -159,6 +159,55 @@ describe('HTTPS in laptop experience w/ pfx', function() {
 
 });
 
+describe('HTTPS in laptop experience w/ pfx obfuscated password', function() {
+
+  var request, httprequest;
+  before(function(done) {
+    process.env.CONFIG_DIR = __dirname + '/definitions/https/httpsexplicit';
+    process.env.NODE_ENV = 'production';
+    process.env.TLS_SERVER_CONFIG = __dirname + '/support/https/tlsconfig-pfx-obfuscated.json'
+    mg.start(3000)
+      .then(function() {
+        return echo.start(8889);
+      })
+      .then(function() {
+        request = supertest(mg.app);
+        httprequest = supertest('http://localhost:3000');
+      })
+      .then(done)
+      .catch(function(err) {
+        console.error(err);
+        done(err);
+      });
+  });
+
+  after(function(done) {
+    mg.stop()
+      .then(function() { return echo.stop(); })
+      .then(done, done)
+      .catch(done);
+    delete process.env.CONFIG_DIR;
+    delete process.env.NODE_ENV;
+    delete process.env.TLS_SERVER_CONFIG;
+  });
+
+  it('should expect success', function(done) {
+    request
+      .get('/https/https')
+      .expect(200, done);
+  });
+  it('should expect failure', function(done) {
+    httprequest
+      .get('/http/http')
+      .end(function(err, res) {
+        if (err) return done(); // expect error
+        done(new Error('expect error'));
+      });
+  });
+
+});
+
+
 describe('HTTPS in laptop experience w/ default TLS', function() {
 
   var request, httprequest;
