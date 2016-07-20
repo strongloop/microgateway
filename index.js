15c522ab0edd506e7e765b350292fab17c5a7651
juangmj <juangmj@tw.ibm.com>
2016-07-20 16:45:24 +0800

    eslint index.js

--- index.js
+++ index.js
@@ -10,23 +10,22 @@ var env = {
   NODE_ENV: 'production',
   APIMANAGER_CATALOG: '',
   APIMANAGER_PORT: 443,
-  APIMANAGER_REFRESH_INTERVAL: 15 * 1000 * 60
-};
+  APIMANAGER_REFRESH_INTERVAL: 15 * 1000 * 60 };
 
 try {
   var envjson = YAML.load(path.join(__dirname, '/env.yaml'));
-  Object.keys(envjson).forEach(function (k) {
+  Object.keys(envjson).forEach(function(k) {
     env[k] = envjson[k];
   });
-}
-catch (e) {
+} catch (e) {
   // Would probably be good to log this somehow...
 }
 
-Object.keys(env).forEach(function (k) {
+Object.keys(env).forEach(function(k) {
   // Don't override env variables that were set explicitly
-  if (typeof process.env[k] === 'undefined')
+  if (typeof process.env[k] === 'undefined') {
     process.env[k] = env[k];
+  }
 });
 
 // Should we do any extra sanity checks here?
