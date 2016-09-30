a8f7a45e9862d70a5c7ca238f8bf426b8766ef8b
Clement <cwsu@tw.ibm.com>
2016-09-30 16:43:06 +0800

    monitor local files for laptop experience

--- datastore/server/boot/load-model.js
+++ datastore/server/boot/load-model.js
@@ -184,6 +184,15 @@ module.exports = function(app) {
       if (!err) {
         loadData(app, apimanager, models, definitionsDir, true, uid);
       }
+      if (!apimanager.host) {
+        //monitor the file changes, load data again if any changes
+        fs.watch(definitionsDir,function (event, filename){
+          if (filename !== ".datastore"){
+            logger.debug('File changed in %s%s, reload data', definitionsDir, filename);
+            loadData(app, apimanager, models, definitionsDir, uid);
+          }
+        });
+      }
     });
 };
 
