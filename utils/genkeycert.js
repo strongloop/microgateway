7de5c6df94fe815abc66d2c8aae10375fb8fcb85
Gary Tu <garytu@tw.ibm.com>
2016-11-07 15:45:06 +0800

    Only create key/cert if not exist

--- utils/genkeycert.js
+++ utils/genkeycert.js
@@ -9,28 +9,72 @@ var pem = require('pem');
 var fs = require('fs');
 var path = require('path');
 
-console.log('Generating self-signed certificate & key..');
-pem.createCertificate({ selfSigned: true }, function(err, keys) {
-  if (err) {
-    console.log(err);
-    return;
+var keyPath = path.resolve(__dirname, '../config/key.pem');
+var certPath = path.resolve(__dirname, '../config/cert.pem');
+
+function checkServiceKey(cb) {
+  try {
+    fs.statSync(keyPath);
+    var serviceKey = fs.readFileSync(keyPath);
+    cb(null, serviceKey);
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      // if key.pem not exist, generate the private key
+      console.log("Create the service key 'config/key.pem'");
+      pem.createPrivateKey(2048, function(e, keyData) {
+        if (e) {
+          cb(e);
+        } else {
+          fs.writeFile(keyPath, keyData.key, function(e) {
+            cb(e, e || { newKey: true, keyData: keyData.key });
+          });
+        }
+      });
+    } else {
+      cb(e);
+    }
   }
-  var certPath = path.resolve(__dirname, '../config/cert.pem');
-  var keyPath = path.resolve(__dirname, '../config/key.pem');
-  fs.writeFile(certPath, keys.certificate, function(e) {
-    if (err) {
-      console.error('Error when generating certificate: ' + e);
+}
+
+function createCertificate(keyData, cb) {
+  pem.createCertificate({ serviceKey: keyData }, function(e, certResult) {
+    if (e) {
+      cb(e);
     } else {
-      console.log('Certificate generated at: ' + certPath);
+      fs.writeFile(certPath, certResult.certificate, cb);
     }
   });
+}
 
-  fs.writeFile(keyPath, keys.serviceKey, function(e) {
-    if (err) {
-      console.error('Error when generating service key: ' + e);
-    } else {
-      console.log('Service key generated at: ' + keyPath);
+function createCertificateCB(err) {
+  if (err) {
+    console.log("Error when creating the cert file 'config/cert.pem': " + err);
+    process.exit(1);
+  }
+}
+
+checkServiceKey(function(err, result) {
+  if (err) {
+    console.log("Error when checking/creating the service key file 'config/key.pem': " + err);
+    process.exit(1);
+  }
+  var certFileExist = false;
+  try {
+    fs.statSync(certPath);
+    certFileExist = true;
+  } catch (e) {
+    if (e.code !== 'ENOENT') {
+      console.log("Error when checking the cert file 'config/cert.pem': " + e);
+      process.exit(1);
     }
-  });
-
+  }
+  if (certFileExist) {
+    if (result.newKey) {
+      console.log("Recreate the cert file 'config/cert.pem'");
+      createCertificate(result.keyData, createCertificateCB);
+    }
+  } else {
+    console.log("Create the cert file 'config/cert.pem'");
+    createCertificate(result.keyData, createCertificateCB);
+  }
 });
