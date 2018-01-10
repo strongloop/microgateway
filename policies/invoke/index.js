3b5afbe59ff8fdc6d4cdc668e42607c98ca81e13
king729 <87287864@qq.com>
2018-01-10 16:11:36 +0800

    utf-8 string length for invoke

--- policies/invoke/index.js
+++ policies/invoke/index.js
@@ -166,6 +166,8 @@ function _main(props, context, next, logger, writeDst, tlsProfile) {
       data = String(data);
     }
   }
+  if(typeof data === 'string')
+      data = new Buffer(data,"utf8");
   dataSz = data.length;
 
   // chunked-upload
